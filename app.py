8d2af8ba143c3315391d3037107f1c2a92a186b5
JoaquinYap <joaquincitoyapura@gmail.com>
2025-05-30 19:56:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-from flask import Flask, render_template, request, redirect
 import sqlite3
+from flask import Flask, render_template, request, redirect
+from collections import defaultdict  # Necesario para agrupar insumos por palet
 
 app = Flask(__name__)
-
 DB = 'inventario.db'
 
 def init_db():
@@ -35,8 +35,10 @@ def agregar():
         palet = request.form['palet']
 
         with sqlite3.connect(DB) as conn:
-            conn.execute('INSERT INTO insumos (codigo, subindice, descripcion, cantidad, palet) VALUES (?, ?, ?, ?, ?)',
-                         (codigo, subindice, descripcion, cantidad, palet))
+            conn.execute(
+                'INSERT INTO insumos (codigo, subindice, descripcion, cantidad, palet) VALUES (?, ?, ?, ?, ?)',
+                (codigo, subindice, descripcion, cantidad, palet)
+            )
         return redirect('/')
     return render_template('agregar.html')
 
@@ -91,8 +93,7 @@ def ver_palets():
         agrupado[insumo[5]].append(insumo)
     return render_template('palets.html', palets=agrupado)
 
-
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get("PORT", 5000)) 
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
